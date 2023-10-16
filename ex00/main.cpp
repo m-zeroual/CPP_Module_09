c03f799a37eb558be9c11fda9f82618eb5878b06
Mohammed zeroual <mohammedzeroual200@gmail.com>
2023-10-16 14:59:12 +0100

    change algo

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,90 +6,171 @@
 /*   By: mzeroual <mzeroual@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 12:53:33 by mzeroual          #+#    #+#             */
-/*   Updated: 2023/10/15 20:33:39 by mzeroual         ###   ########.fr       */
+/*   Updated: 2023/10/16 14:56:20 by mzeroual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 std::list<std::string> split(std::string line, std::string delimiter)
 {
-    size_t npos = -1;
-    size_t first = 0;
-    std::list<std::string> parse;
-    
+	size_t npos = -1;
+	size_t first = 0;
+	std::list<std::string> parse;
+	
 
-    size_t pos = line.find(delimiter);
-    if (pos != npos)
-    {
-        while (pos != npos)
-        {
-            parse.push_back(line.substr(0, pos));
-            line = line.substr(pos + 1, line.size());
-            first = pos + 1;
-            pos = line.find(delimiter);
-        }
-        parse.push_back(line.substr(0, line.size()));
+	size_t pos = line.find(delimiter);
+	if (pos != npos)
+	{
+		while (pos != npos)
+		{
+			parse.push_back(line.substr(0, pos));
+			line = line.substr(pos + 1, line.size());
+			first = pos + 1;
+			pos = line.find(delimiter);
+		}
+		parse.push_back(line.substr(0, line.size()));
 
-    }
-    return (parse);
+	}
+	else
+		return (parse.push_back(line), parse);
+	return (parse);
 }
 
-void removeSpace(std::string &remove)
+void trimSpace(std::string &remove)
 {
-    size_t npos = -1;
-    
-    size_t pos = remove.find(" ");
-    if (pos != npos && pos)
-        remove = remove.substr(0, pos);
-    else if (!pos)
-        remove = remove.substr(1, remove.size());
-        
+	size_t	npos = -1;
+	size_t	i;
+
+	for (i = 0; i < remove.size(); i++)
+	{
+		if (remove[i] != ' ') {
+			break ;
+		}
+	}
+	remove = remove.substr(i, remove.size());
+	size_t end = remove.find(" ");
+	if (end != npos)
+		remove = remove.substr(0, end);
+		
+	std::cout << "=" << remove << "=" << std::endl;
+	
 }
 int isNumber(std::string number)
 {
-    for (size_t i = 0; i < number.size(); i++)
-    {
-        if (!isdigit(number[i]))
-            return (0);
-    }
-    return (1);
+	for (size_t i = 0; i < number.size(); i++)
+	{
+		if (!isdigit(number[i]))
+			return (0);
+	}
+	return (1);
 }
+void parseGeneralError(std::list<std::string> lineSplit, int lineNum)
+{
+	std::list<std::string>::iterator firstIt = lineSplit.begin();
+	int i = 0;
+	for (std::list<std::string>::iterator it = firstIt; it != lineSplit.end(); it++)
+	{
+		// trimSpace(*it);
+		if (lineSplit.size() != 2 || (it == firstIt && ((*it).size() != 11 || (*it)[10] != ' '))) {
+			std::cerr << "Error : in line " << lineNum << " date not valid\n";
+			break ;
+		}
+		else if ((++it)-- == lineSplit.end() && (((*it).size() != 2 && (*it).size() != 3 && (*it).size() != 4 && (*it).size() != 5)
+				|| (*it)[0] != ' ' || (*it)[1] == ' ')) {
+			std::cerr << "Error : in line " << lineNum << " value not valid\n";
+			break ;
+		}
+		trimSpace(*it);
+		if (it == firstIt) {
+			std::list<std::string> date = split(*it, "-");
+			if (date.size() != 3) {
+				std::cerr << "Error : in line " << lineNum << " date not valid\n";
+			}
+			for (std::list<std::string>::iterator it1 = date.begin(); it1 != date.end(); it1++)
+			{
+				if (!isNumber(*it1)) {
+					if (i == 0)
+						std::cerr << "Error : in line " << lineNum << " Year not a number\n";
+					else if (i == 1)
+						std::cerr << "Error : in line " << lineNum << " Month not a number\n";
+					else if (i == 2)
+						std::cerr << "Error : in line " << lineNum << " Day not a number\n";
+				}
+				i ++;
+			}
+			
+		}
+		
+	}
+}
+
 void prseLine(std::string line, int lineNum)
 {
-    (void)lineNum;
-    std::list<std::string> date;
-    
-    if (!line.empty()) {
-        std::list<std::string> lineSplit = split(line, "|");
-        std::list<std::string>::iterator firstIt = lineSplit.begin();
-        
-        for (std::list<std::string>::iterator it = firstIt; it != lineSplit.end(); it++)
-        {
-            if (lineNum == 2)
-                exit (0);
-            removeSpace(*it);
-            if (lineNum != 0)
-                std::cout << "#" << *it << "#\n";
-            if (it == firstIt) {
-                date = split(*it, "-");
-            }
-            for (std::list<std::string>::iterator it1 = date.begin(); it1 != date.end(); it1++)
-            {
-                if (lineNum == 2)
-                    exit (0);
-                std::cout << "$" << *it1 << "$\n";
-                if (!isNumber(*it1))
-                    std::cerr << "Error : in line " << lineNum << " date not valid"<< std::endl;
-                    
-                // if ((++it1) ==  paseFisrtPart.end())
-                //     std::cout << std::endl;
-                // it1--;
-            }
-            // break ;
-            // std::cout << "#\n";
-            // std::cout << std::endl;
-        }
-    }
+	std::list<std::string> date;
+	
+	if (!line.empty()) {
+		std::list<std::string> lineSplit = split(line, "|");
+		parseGeneralError(lineSplit, lineNum);
+	}
+}
+		// for (std::list<std::string>::iterator it = firstIt; it != lineSplit.end(); it++)
+		// {
+			// if (lineSplit.size() != 2 || (it == firstIt && ((*it).size() != 11 || (*it)[10] != ' '))) {
+			//     std::cerr << "Error : in line " << lineNum << " date not valid\n";
+			//     break ;
+			// }
+			// else if ((++it)-- == lineSplit.end() && (((*it).size() != 2 && (*it).size() != 3 && (*it).size() != 4 && (*it).size() != 5)
+			//         || (*it)[0] != ' ' || (*it)[1] == ' ')) {
+			//     std::cerr << "Error : in line " << lineNum << " value not valid\n";
+			//     break ;
+			// }
+		
+			// std::cout << "\t#" << *it << "#\n";
+			// if (lineNum == 2)
+			//     exit (0);
+			// std::cout << "=" << (*it).size() << "=\n";
+			// removeSpace(*it);
+			// if (lineNum != 0)
+			// if (it == firstIt) {
+			//     date = split(*it, "-");
+			//     for (std::list<std::string>::iterator it1 = date.begin(); it1 != date.end(); it1++)
+			//     {
+			//         if (lineNum == 2)
+			//             exit (0);
+			//         std::cout << "$" << *it1 << "$\n";
+			//         // std::cout << "@" << date.size() << "@\n";
+			//         if (date.size() != 3 || !isNumber(*it1))
+			//         {
+			//             std::cerr << "Error : in line " << lineNum << " date not valid"<< std::endl;
+			//             break;
+			//         }
+			//         std::cout << "@" << *it1 << "@\n";
+
+			//         // if ((++it1) ==  paseFisrtPart.end())
+			//         //     std::cout << std::endl;
+			//         // it1--;
+			//     }
+			// }
+			// for (std::list<std::string>::iterator it1 = date.begin(); it1 != date.end(); it1++)
+			// {
+			//     if (lineNum == 2)
+			//         exit (0);
+			//     std::cout << "$" << *it1 << "$\n";
+			//     // std::cout << "$" << date.size() << "$\n";
+			//     if (date.size() != 3 || !isNumber(*it1))
+			//     {
+			//         std::cerr << "Error : in line " << lineNum << " date not valid"<< std::endl;
+			//         break;
+			//     }
+			//     // if ((++it1) ==  paseFisrtPart.end())
+			//     //     std::cout << std::endl;
+			//     // it1--;
+			// }
+			// break ;
+			// std::cout << "#\n";
+			// std::cout << std::endl;
+		// }
+// }
 //     for (size_t i = 0; i < 14; i++)
 //     {
 //         // int j = 0;
@@ -102,7 +183,7 @@ void prseLine(std::string line, int lineNum)
 //                     std::cerr << "Error : in line " << lineNum << " date format is must be like (Year-Month-Day)"<< std::endl;
 //                     break ;
 //                 }
-                
+				
 //                 // else if (!isNumber(*it1))
 //                 // {
 //                 //     if (paseFisrtPart.size() == 0)
@@ -115,7 +196,7 @@ void prseLine(std::string line, int lineNum)
 //                 // std::cout << "$" << *it1 << "$ ";
 //                 // if (!isNumber(*it1))
 //                 //     std::cerr << "Error : in line " << lineNum << " date not a number"<< std::endl;
-                    
+					
 //                 // if ((++it1) ==  paseFisrtPart.end())
 //                 //     std::cout << std::endl;
 //                 // it1--;
@@ -128,44 +209,47 @@ void prseLine(std::string line, int lineNum)
 //         }
 //     }
 
-}
+// }
 
 void parseInputFile(std::list<std::string> inputContant)
 {
-    int i = 0;
-    std::list<std::string>::iterator first = inputContant.begin();
-    for (std::list<std::string>::iterator it = first; it != inputContant.end(); it++)
-    {
-        if (it == first) {
-            if (*first != "date | value")
-                std::cerr << "Error : in head of input file\n";
-        }
-        prseLine(*it, i);
-        i++;
-    }
+	int i = 0;
+	std::list<std::string>::iterator first = inputContant.begin();
+	for (std::list<std::string>::iterator it = first; it != inputContant.end(); it++)
+	{
+		if (it == first) {
+			if (*first != "date | value")
+				std::cerr << "Error : in head of input file\n";
+		}
+		else
+			prseLine(*it, i);
+		if (i == 1)
+			break ;
+		i++;
+	}
 }
 
 int main(int ac, char *av[])
 {
-    if (ac == 2)
-    {
-        std::list<std::string> input;
-        std::string content;
-        
-        std::ifstream fileName;
-        fileName.open(av[1]);
-        if (!fileName) {                            
-            std::cout << "Error : opening the file\n";          
-        }
-        else {
-            while (std::getline(fileName, content, '\n')) {
-                input.push_back(content);
-            }
-            parseInputFile(input);
-            fileName.close();                   
-        }
-    }
-    else
-        std::cerr << "Error : You need file name" << std::endl;
-    return 0;
+	if (ac == 2)
+	{
+		std::list<std::string> input;
+		std::string content;
+		
+		std::ifstream fileName;
+		fileName.open(av[1]);
+		if (!fileName) {                            
+			std::cout << "Error : opening the file\n";          
+		}
+		else {
+			while (std::getline(fileName, content, '\n')) {
+				input.push_back(content);
+			}
+			parseInputFile(input);
+			fileName.close();                   
+		}
+	}
+	else
+		std::cerr << "Error : You need file name" << std::endl;
+	return 0;
 }
