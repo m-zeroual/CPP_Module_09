58c98293271fb225df9337d4825accb9b62cacdc
Mohammed zeroual <mohammedzeroual200@gmail.com>
2023-10-21 13:04:26 +0100

    fix all parse in ex01

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: mzeroual <mzeroual@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 10:26:23 by mzeroual          #+#    #+#             */
-/*   Updated: 2023/10/20 12:47:13 by mzeroual         ###   ########.fr       */
+/*   Updated: 2023/10/21 13:01:34 by mzeroual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 #define RPN_HPP
 
 #include <iostream>
+#include <stack>
+#include <sstream>
+
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,110 +6,64 @@
 /*   By: mzeroual <mzeroual@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 11:45:08 by mzeroual          #+#    #+#             */
-/*   Updated: 2023/10/20 21:50:39 by mzeroual         ###   ########.fr       */
+/*   Updated: 2023/10/21 13:03:16 by mzeroual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
-#include <stack>
 
-std::string	trimSpace(std::string remove, char c)
+void calcul(std::stack<int> &s1, char op)
 {
-	size_t	i;
+	int rhs = s1.top();
+	s1.pop();
+	int lhs = s1.top();
+	s1.pop();
 
-	for (i = 0; i < remove.size(); i++)
-		if (remove[i] != c)
-			break ;
-	remove = remove.substr(i, remove.size());
-	for (i = remove.size() -1; i >= 0; i--)
-		if (remove[i] != ' ')
-			break ;
-	remove = remove.substr(0, i + 1);
-	return (remove);
+	if (op == '+')
+		s1.push(lhs + rhs);
+	else if (op == '-')
+		s1.push(lhs - rhs);
+	else if (op == '*')
+		s1.push(lhs * rhs);
+	else if (op == '/')
+		s1.push(lhs / rhs);
 }
 
-std::stack<std::string>	split(std::string line, char delimiter)
+int  parseInput(std::string arg)
 {
-	std::stack<std::string> parse;
+	std::stack<int> s1;
+	std::stringstream s(arg);
+	std::string word;
 	
-	size_t pos = line.find(delimiter);
-	if (pos != std::string::npos)
+	while (s >> word)
 	{
-		while (pos != std::string::npos)
+		if (word.length() > 1)
+			return (std::cout << "Error\n", 0);
+		else if (!isdigit(word[0]) && word[0] != '-' && word[0] != '+' && word[0] != '*' && word[0] != '/')
+			return (std::cout << "Error\n", 0);
+			
+		if (isnumber(word[0]))
+			s1.push(std::atoi(word.c_str()));
+		else if (word[0] == '+' || word[0] == '-' || word[0] == '*' || word[0] == '/')
 		{
-			parse.push(line.substr(0, pos));
-			line = line.substr(pos + 1, line.size());
-			line = trimSpace(line, delimiter);
-			pos = line.find(delimiter);
+			if (s1.size() < 2)
+				return (std::cout << "Error : syntax!!\n", 0);
+			else
+				calcul(s1, word[0]);
 		}
-		parse.push(line.substr(0, line.size()));
 	}
-	else
-		return (parse.push(line), parse);
-	return (parse);
-}
-
-void parseArgs(std::string element)
-{
-	char *end = NULL;
-	long n = strtol(element.c_str(), &end, 10);
-	// if (*end && (strlen(end) >= 2
-	// || (strlen(end) == 1 && *end != '+' && *end != '-' && *end != '*' && *end != '/')
-	// || (strlen(end) == 1 && isprint(*end))))
-	// if (*end && (strlen(end) >= 2 && isprint(*end) && (*end != '+' && *end != '-' && *end != '*' && *end != '/')))
-	// || (strlen(end) == 1 && *end != '+' && *end != '-' && *end != '*' && *end != '/')))
-
-
-	//  || strlen(end) >= 2 || ((*end != '+' && *end != '-' && *end != '*' && *end != '/') || n)))
-	if (*end && (strlen(end) >= 2 || element.length() != strlen(end) || (*end != '+' && *end != '-' && *end != '*' && *end != '/') ))
-	{
-		std::cout << "Error : not a number!!\n";
-		exit (1);
-	}
-	// if (strlen(end) == 1 && (*end == '+' || *end == '-' || *end == '*' || *end == '/'))
-	//     countOp++;
-	if (n < 0)
-	{
-		std::cout << "Error : number is negative!!\n";
-		exit (1);
-	}
-}
-// void checkSyntax(std::string args)
-// {
-//     std::cout <<  "#" << args << "#" << std::endl;
-// }
-
-void readInput(std::string args)
-{
-	args = trimSpace(args, ' ');
-	std::stack<std::string> args_split = split(args, ' ');
-	std::stack<std::string> copy(args_split);
-
-	// checkSyntax(args);
-	size_t len = args_split.size();
-	for (size_t i = 0; i < len; i++)
-	{
-		std::string element = args_split.top();
-		std::cout << "|" << element << "|" << std::endl;
-		parseArgs(element);
-		args_split.pop();
-	}
-	args_split = copy;
-	// for (size_t i = 0; i < len; i++)
-	// {
-	// 	std::string element = args_split.top();
-	// 	if ()
-	// 	// std::cout << "|" << element << "|" << std::endl;
-	// 	parseArgs(element);
-	// 	args_split.pop();
-	// }
+	if (s1.size() != 1)
+		return (std::cout << "Error : syntax!!\n", 0);
+	std::cout << s1.top() << std::endl;
+	return (1);
 }
 
 int main(int ac, char const *av[])
 {
 	if (ac == 2)
 	{
-		readInput(av[1]);
+		if (!parseInput(av[1]))
+			exit (1);
 	}
 	else
 		std::cout << "bad args!!\n";
