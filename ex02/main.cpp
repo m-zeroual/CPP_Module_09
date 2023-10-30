e12739e507dcc41db058d147bded210b490594cc
Mohammed zeroual <mohammedzeroual200@gmail.com>
2023-10-30 12:30:30 +0100

    merge-insertion sort in 66

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mzeroual <mzeroual@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 13:51:45 by mzeroual          #+#    #+#             */
-/*   Updated: 2023/10/30 11:35:03 by mzeroual         ###   ########.fr       */
+/*   Updated: 2023/10/30 12:29:55 by mzeroual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,12 @@ int parse(char *av)
 		return (std::cout << "Error : not a positive number.\n", 0);
 	return (1);
 }
+
 void display(std::vector <int> d)
 {
 	for (size_t i = 0; i < d.size(); i++)
 		std::cout << d[i] << " ";
+	std::cout << std::endl;
 }
 
 void swapPair(pair &p)
@@ -70,6 +72,7 @@ vPair merge(std::vector<int> v, std::vector<int> &rest)
 	}
 	return(vp);
 }
+
 void sort(vPair &vp)
 {
 	size_t size = vp.size();
@@ -80,14 +83,17 @@ void sort(vPair &vp)
 			swapPair(vp[i]);
 	}
 }
+
 bool comp(std::vector<int> a, std::vector<int> b)
 {
 	contComp++;
 	return (a.back() < b.back());
 }
+
 std::vector<int> convertFromPairToVector(vPair vP)
 {
-	std::vector<int> v; 
+	std::vector<int> v;
+
 	for (size_t i = 0; i < vP.size(); i++)
 	{
 		for (size_t j = 0; j < vP[i].first.size(); j++)
@@ -102,6 +108,7 @@ std::vector<int> convertFromPairToVector(vPair vP)
 std::vector<int> convertToVector(vVector v)
 {
 	std::vector<int> v1;
+
 	for (size_t i = 0; i < v.size(); i++)
 	{
 		for (size_t j = 0; j < v[i].size(); j++)
@@ -112,7 +119,7 @@ std::vector<int> convertToVector(vVector v)
 
 void insertion(std::vector<int> &v, std::vector<int> rest)
 {
-	std::cout << ">>>>> REV <<<<<" << std::endl;
+	// std::cout << ">>>>> REV <<<<<" << std::endl;
 	pair							p;
 	vPair							vp;
 	std::vector<int>				v1;
@@ -121,7 +128,6 @@ void insertion(std::vector<int> &v, std::vector<int> rest)
 	std::vector< std::vector<int> >	paned;
 
 	size_t c = 1 << mergeCount--;
-
 	size_t size = v.size() / c * c;
 	for (size_t i = 0; i < size; i+=c) {
 		for (size_t j = 0; j < c / 2; j++) 
@@ -133,57 +139,33 @@ void insertion(std::vector<int> &v, std::vector<int> rest)
 		v1.clear();
 		v2.clear();
 	}
-	// std::cout << "vp size\t: " << vp.size() << std::endl;
 	for (size_t i = 0; i < vp.size(); i++) {
 		paned.push_back(vp[i].first);
 		mchain.push_back(vp[i].second);
 	}
 	if (rest.size())
 		paned.push_back(rest);
-
 	mchain.insert(mchain.begin(), *paned.begin());
-	// if (paned.size() >= 2)
-	// 	mchain.insert(std::lower_bound(mchain.begin(), mchain.begin() + 2, paned[1], comp), paned[1]);
-
 	int countPush = 0;
 	size = mchain.size();
 	for (size_t i = 1; i < paned.size(); i++) {
-			size_t jacobsthal = (pow(2, ((i+1) + 1)) + pow(-1, (i+1))) / 3;						//(2k+1 + (−1)k)/3
+			size_t jacobsthalStart = (pow(2, ((i+1) + 1)) + pow(-1, (i+1))) / 3;						//(2k+1 + (−1)k)/3
 			size_t jacobsthalEnd = ((pow(2, ((i+1) - 1) + 1) + pow(-1, ((i+1) - 1))) / 3) + 1;	//(2(k-1)+1 + (−1)(k-1))/3 => Btk−1+1
-			// std::cout << "s = " << jacobsthal << std::endl;
-			// std::cout << "e = " << jacobsthalEnd << std::endl;
-			if (jacobsthal > paned.size())
-				jacobsthal = paned.size();
-
-			// if (jacobsthalEnd > paned.size())
-			// 	break ;
-			for (size_t j = jacobsthal; j >= jacobsthalEnd ; j--)
-			{
-				std::cout << "jacob = " << jacobsthal << std::endl;
-				std::cout << "j     = " << j << std::endl;
-				// std::cout << "e = " << jacobsthalEnd << std::endl;
-				// std::cout << "j = " << (jacobsthal - j) << std::endl;
-				// std::cout << "j = " << (jacobsthalEnd - j) << std::endl;
-				// std::cout << "j = " << (jacobsthal - jacobsthalEnd) << std::endl;
-				// std::cout << "j = " << mchain.end() -1 - mchain.begin() << std::endl;
-				countPush =  jacobsthal + (mchain.size() - size);
-				std::cout << "bc = " << countPush << std::endl;
-				// if ()
-				// std::cout << "c = " << countPush << std::endl;
+			if (jacobsthalStart > paned.size())
+				jacobsthalStart = paned.size();
+			if (jacobsthalEnd > paned.size())
+				break ;
+			for (size_t j = jacobsthalStart; j >= jacobsthalEnd ; j--) {
+				countPush = jacobsthalStart + (mchain.size() - size) - (jacobsthalStart - j);
 				mchain.insert(std::lower_bound(mchain.begin(), mchain.begin() + countPush , paned[j-1], comp), paned[j-1]);
-				// countPush +=  (jacobsthal - j);
-				// std::cout << "ac = " << countPush << std::endl;
-				// countPush--;
 			}
-			// countPush--;
-			// mchain.insert(std::lower_bound(mchain.begin(), mchain.end(), paned[i], comp), paned[i]);
 	}
 	v = convertToVector(mchain);
 }
 
 void recursion(std::vector<int> &v)
 {
-	std::cout << ">>>>> REC <<<<<" << std::endl;
+	// std::cout << ">>>>> REC <<<<<" << std::endl;
 	std::vector<int>	rest;
 
 	vPair vp = merge(v, rest);
@@ -197,7 +179,6 @@ void recursion(std::vector<int> &v)
 int main(int ac, char *av[])
 {
 	std::vector<int>    numbers;
-
 	if (ac > 1)
 	{
 		av = av + 1;
@@ -209,14 +190,10 @@ int main(int ac, char *av[])
 		}
 		std::cout << "befor = ";
 		display(numbers);
-		std::cout << "\n";
-		
 		recursion(numbers);
-		std::cout << "after =";
+		std::cout << "after = ";
 		display(numbers);
-		std::cout<< "\n";
-		std::cout << std::endl;
-		// std::cout << "comp : " << contComp << std::endl; 
+		std::cout << "comp : " << contComp << std::endl; 
 	}
 	else
 		std::cout << "give me positive numbers to sort.\n";
