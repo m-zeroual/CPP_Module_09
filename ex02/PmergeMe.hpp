d5a6a45618e27efc4326b7e5c2605e94346e1939
Mohammed zeroual <mohammedzeroual200@gmail.com>
2023-10-30 17:55:02 +0100

    ready to push

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: mzeroual <mzeroual@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 13:52:21 by mzeroual          #+#    #+#             */
-/*   Updated: 2023/10/30 16:08:44 by mzeroual         ###   ########.fr       */
+/*   Updated: 2023/10/30 17:50:41 by mzeroual         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,7 @@
 #include <algorithm>
 #include <utility>
 
-
-// std::deque
+// std::vector
 typedef std::pair< std::vector<int>, std::vector<int> > pairVector;
 typedef std::vector<pairVector>                         vectorPair;
 typedef std::vector<std::vector<int> >                  vectorVector;
@@ -31,5 +30,57 @@ typedef std::vector<std::vector<int> >                  vectorVector;
 typedef std::pair< std::deque<int>, std::deque<int> >   pairDeque;
 typedef std::deque<pairDeque>                           dequePair;
 typedef std::deque< std::deque<int> >                   dequeDeque;
+
+// ---------- template function -------------
+template<typename T>
+void display(std::string s, T arrP)
+{
+	std::cout << s;
+	for (size_t i = 0; i < arrP.size(); i++)
+		std::cout << arrP[i] << " ";
+	std::cout << std::endl;
+}
+template<typename T>
+void swapPair(T &p)
+{
+	T	swap;
+
+	swap.first = p.second;
+	swap.second = p.first;
+	p.first = swap.first;
+	p.second = swap.second;
+}
+template<typename T>
+bool comp(T a, T b)
+{
+	return (a.back() < b.back());
+}
+template<typename T>
+void sort(T &arrP)
+{
+	size_t size = arrP.size();
+	for (size_t i = 0; i < size; i++)
+	{
+		if (comp(arrP[i].second, arrP[i].first))
+			swapPair(arrP[i]);
+	}
+}
+
+int parse(char *av);
+
+// ------------------------- vector ---------------------------------
+vectorPair merge(std::vector<int> v, std::vector<int> &rest);
+std::vector<int> convertFromPairToVector(vectorPair vP);
+std::vector<int> convertToVector(vectorVector v);
+void insertion(std::vector<int> &v, std::vector<int> rest);
+void recursion(std::vector<int> &v);
+
+
+// ------------------------- deque ---------------------------------
+dequePair merge(std::deque<int> v, std::deque<int> &rest);
+std::deque<int> convertFromPairToDeque(dequePair vP);
+std::deque<int> convertToDeque(dequeDeque v);
+void insertion(std::deque<int> &v, std::deque<int> rest);
+void recursion(std::deque<int> &v);
 
 #endif
